2b41ef0c7e8abbdc7c9200f2a926fabc96d2697b
ZARDeonBotha <darkwingduck105@icloud.com>
2025-07-20 13:56:38 +0200

    Added dot env import and package

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # X (Twitter) API credentials loaded from environment variables
 X_BEARER_TOKEN = os.environ.get('X_BEARER_TOKEN', '')
